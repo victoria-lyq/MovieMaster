2953b4b465986d50807b369d77e90e868ddd095c
yiranwangg <wang.yiran@northeastern.edu>
2021-11-17 13:17:04 -0800

    add auto generate for user

---
+++ java/movie/model/Users.java
@@ -0,0 +1,95 @@
+package movie.model;
+
+public class Users {
+	protected Integer userId;
+	protected String password;
+	protected String userName;
+	protected String firstName;
+	protected String lastName;
+	protected String email;
+	protected String phone;
+	
+	public Users(Integer userId, String password, String userName, String firstName, String lastName, String email, String phone) {
+		this.userId = userId;
+		this.password = password;
+		this.userName = userName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+		
+	}
+	
+
+	public Users(String password, String userName, String firstName, String lastName, String email, String phone) {
+		super();
+		this.password = password;
+		this.userName = userName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+	}
+
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Users(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
